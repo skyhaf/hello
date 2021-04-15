b341c47280d047577aad224c16af31c80cc8056d
skyhaf <hafiznasution2002@gmail.com>
2021-04-16 00:33:48 +0700

    landing page halo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="flex justify-start p-32 bg-blue-900 mt-24 dark:bg-gray-800">
+      <p className="font-bold text-white text-2xl ">
+        Halo semuanya, saya
+        <a
+          href="https://www.linkedin.com/in/hafizbhadrikaalamsyah/"
+          target="_blank"
+          className="p-1 text-blue-300 hover:text-blue-400 hover:underline cursor-pointer animate-pulse"
+        >
+          Hafiz Bhadrika Alamsyah
+        </a>
+      </p>
+      
+    </div>
+  );
+}
+
+export default App;
