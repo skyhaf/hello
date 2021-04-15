cf5ff78767b54a4f6adf4e9525c754f956585255
skyhaf <hafiznasution2002@gmail.com>
2021-04-16 01:26:56 +0700

    try push heroku

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Components/home";
+
 function App() {
   return (
-    <div className="flex justify-start p-32 bg-blue-900 mt-24 dark:bg-gray-800">
-      <p className="font-bold text-white text-2xl ">
-        Halo semua, saya
-        <a
-          href="https://www.linkedin.com/in/hafizbhadrikaalamsyah/"
-          target="_blank"
-          className="p-1 text-blue-300 hover:text-blue-400 hover:underline cursor-pointer animate-pulse"
-        >
-          Hafiz Bhadrika Alamsyah
-        </a>
-      </p>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
