034fadf2133f133b6ef88e0c9eec0bed81d0f643
skyhaf <hafiznasution2002@gmail.com>
2021-07-04 04:30:32 +0700

    update responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,11 @@ function App() {
     <Switch>
       <Route exact path="/">
         <div className="hidden lg:contents relative">
-
-          {/* <div className="fixed -z-10" id="particles-js"></div> */}
           <Home className="z-10" />
         </div>
 
         <div className="bg-primary w-screen min-h-screen lg:hidden flex text-white text-3xl font-head-border text-center justify-center items-center">
-          Untuk pengalaman yang lebih nyaman, anda bisa buka di tablet/laptop/pc
+          For better experience, please open in your PC/Laptop.
         </div>
       </Route>
     </Switch>
